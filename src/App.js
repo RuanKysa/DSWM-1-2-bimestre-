6e96c2c90f0921185645f3b55192602c0a93790e
RuanKysa <ruancjbueno@gmail.com>
2024-08-06 17:56:37 -0300

    Nutri

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css"
+export default function App() {
+  const [nutri, setNutri] = useState([]);
 
-function App() {
+  useEffect(() => {
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => setNutri(json))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
       </header>
+      {nutri.map((item) => (
+        <article key={item.id}>
+          <strong>{item.titulo}</strong>
+          <img src={item.capa} alt={item.titulo} />
+          <p>{item.subtitulo}</p>
+          <button>Acessar</button>
+        </article>
+      ))}
     </div>
   );
 }
-
-export default App;
